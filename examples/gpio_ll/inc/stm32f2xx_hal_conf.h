34444066a275ce698022abcb31223a9bbbbf079b
Jorge Troncoso <jatron@mit.edu>
2017-12-16 20:43:58 -0500

    cleanup

--- examples/gpio_ll/inc/stm32f2xx_hal_conf.h
+++ examples/gpio_ll/inc/stm32f2xx_hal_conf.h
@@ -6,9 +6,6 @@
  extern "C" {
 #endif
 
-/* Exported types ------------------------------------------------------------*/
-/* Exported constants --------------------------------------------------------*/
-
 /* ########################## Module Selection ############################## */
 /**
   * @brief This is the list of modules to be used in the HAL driver 
