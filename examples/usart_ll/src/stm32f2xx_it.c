04ca300b4a4b2be21f920d85b728e9a067a69156
Jorge Troncoso <jatron@mit.edu>
2017-12-16 21:45:16 -0500

    cleanup

--- examples/usart_ll/src/stm32f2xx_it.c
+++ examples/usart_ll/src/stm32f2xx_it.c
@@ -1,60 +1,5 @@
-/** 
-  ******************************************************************************
-  * @file    Examples_LL/USART/USART_Communication_Rx_IT/Src/stm32f2xx_it.c
-  * @author  MCD Application Team
-  * @version V1.0.0
-  * @date    17-March-2017
-  * @brief   Main Interrupt Service Routines.
-  *          This file provides template for all exceptions handler and
-  *          peripherals interrupt service routine.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT(c) 2017 STMicroelectronics</center></h2>
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
-
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f2xx_it.h"
-
-/** @addtogroup STM32F2xx_LL_Examples
-  * @{
-  */
-
-/** @addtogroup USART_Communication_Rx_IT
-  * @{
-  */
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-
-/* Private function prototypes -----------------------------------------------*/
-/* Private functions ---------------------------------------------------------*/
 
 /******************************************************************************/
 /*            Cortex-M3 Processor Exceptions Handlers                         */
@@ -62,8 +7,6 @@
 
 /**
   * @brief  This function handles NMI exception.
-  * @param  None
-  * @retval None
   */
 void NMI_Handler(void)
 {
@@ -71,8 +14,6 @@ void NMI_Handler(void)
 
 /**
   * @brief  This function handles Hard Fault exception.
-  * @param  None
-  * @retval None
   */
 void HardFault_Handler(void)
 {
@@ -84,8 +25,6 @@ void HardFault_Handler(void)
 
 /**
   * @brief  This function handles Memory Manage exception.
-  * @param  None
-  * @retval None
   */
 void MemManage_Handler(void)
 {
@@ -97,8 +36,6 @@ void MemManage_Handler(void)
 
 /**
   * @brief  This function handles Bus Fault exception.
-  * @param  None
-  * @retval None
   */
 void BusFault_Handler(void)
 {
@@ -110,8 +47,6 @@ void BusFault_Handler(void)
 
 /**
   * @brief  This function handles Usage Fault exception.
-  * @param  None
-  * @retval None
   */
 void UsageFault_Handler(void)
 {
@@ -123,8 +58,6 @@ void UsageFault_Handler(void)
 
 /**
   * @brief  This function handles SVCall exception.
-  * @param  None
-  * @retval None
   */
 void SVC_Handler(void)
 {
@@ -132,8 +65,6 @@ void SVC_Handler(void)
 
 /**
   * @brief  This function handles Debug Monitor exception.
-  * @param  None
-  * @retval None
   */
 void DebugMon_Handler(void)
 {
@@ -141,8 +72,6 @@ void DebugMon_Handler(void)
 
 /**
   * @brief  This function handles PendSVC exception.
-  * @param  None
-  * @retval None
   */
 void PendSV_Handler(void)
 {
@@ -150,8 +79,6 @@ void PendSV_Handler(void)
 
 /**
   * @brief  This function handles SysTick Handler.
-  * @param  None
-  * @retval None
   */
 void SysTick_Handler(void)
 {
@@ -166,8 +93,6 @@ void SysTick_Handler(void)
 
 /**
   * @brief  This function handles external lines 15 to 10 interrupt request.
-  * @param  None
-  * @retval None
   */
 void USER_BUTTON_IRQHANDLER(void)
 {
@@ -184,8 +109,6 @@ void USER_BUTTON_IRQHANDLER(void)
 
 /**
   * Brief   This function handles USARTx Instance interrupt request.
-  * Param   None
-  * Retval  None
   */
 void USARTx_IRQHandler(void)
 {
@@ -201,15 +124,4 @@ void USARTx_IRQHandler(void)
     /* Call Error function */
     Error_Callback();
   }
-	
 }
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
