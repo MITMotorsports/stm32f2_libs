3be88a3da091ce010e6280c6e2dc60d6075e95ec
Jorge Troncoso <jatron@mit.edu>
2017-12-16 21:54:10 -0500

    cleanup

--- examples/usart_ll/inc/stm32f2xx_it.h
+++ examples/usart_ll/inc/stm32f2xx_it.h
@@ -2,18 +2,10 @@
 #ifndef __STM32F2xx_IT_H
 #define __STM32F2xx_IT_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* Exported types ------------------------------------------------------------*/
-/* Exported constants --------------------------------------------------------*/
-/* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
-
 void NMI_Handler(void);
 void HardFault_Handler(void);
 void MemManage_Handler(void);
@@ -26,10 +18,4 @@ void SysTick_Handler(void);
 void USER_BUTTON_IRQHANDLER(void);
 void USARTx_IRQHandler(void);
 
-#ifdef __cplusplus
-}
-#endif
-
 #endif /* __STM32F2xx_IT_H */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
